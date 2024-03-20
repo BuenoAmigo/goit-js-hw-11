c5e01bd2653a66b48c79db23991026eb8859091a
Illia <kudrya377@gmail.com>
2024-03-20 22:01:26 +0000

    commit_init

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+import iziToast from "izitoast";
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "izitoast/dist/css/iziToast.min.css";
