e2ce8ed4fb91f65ae270802c3f8ead04e878a2f1
Illia <kudrya377@gmail.com>
2024-04-02 18:14:57 +0100

    commit_003
    All fixed.
    Import/export is done.
    Main.js contains main logic, no comments or unneccesary code.

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,52 +1,48 @@
-// // У файлі render-functions.js створи функції для відображення елементів інтерфейсу.
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
 
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-
-
-// export function createGalleryMarkup(arr) {
-//     const galleryList = document.querySelector(".gallery");
-//     const markup = arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-//    <div class="galleryItem-wrapper">
-//         <div class="galleryCard-wrapper">
-//             <li class="gallery_items">
-//                 <a href="${largeImageURL}">
-//                 <img src="${webformatURL}" alt="${tags}" class="galleryItems-photo"></a>
-//                 <div class="gallery-textWrapper">
-//                     <ul class="galleryItems-list">
-//                         <li class="galleryItem">
-//                             <h2>Likes</h2>
-//                             <p>${likes}</p>
-//                         </li>
-//                         <li class="galleryItem">
-//                             <h2>Views</h2>
-//                             <p>${views}</p>
-//                         </li>
-//                         <li class="galleryItem">
-//                             <h2>Comments</h2>
-//                             <p>${comments}</p>
-//                         </li>
-//                         <li class="galleryItem">
-//                             <h2>Downloads</h2>
-//                             <p>${downloads}</p>
-//                         </li>
-//                     </ul>
-//                 </div>
+export function createGalleryMarkup(arr) {
+    const galleryList = document.querySelector(".gallery");
+    const markup = arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+   <div class="galleryItem-wrapper">
+        <div class="galleryCard-wrapper">
+            <li class="gallery_items">
+                <a href="${largeImageURL}">
+                <img src="${webformatURL}" alt="${tags}" class="galleryItems-photo"></a>
+                <div class="gallery-textWrapper">
+                    <ul class="galleryItems-list">
+                        <li class="galleryItem">
+                            <h2>Likes</h2>
+                            <p>${likes}</p>
+                        </li>
+                        <li class="galleryItem">
+                            <h2>Views</h2>
+                            <p>${views}</p>
+                        </li>
+                        <li class="galleryItem">
+                            <h2>Comments</h2>
+                            <p>${comments}</p>
+                        </li>
+                        <li class="galleryItem">
+                            <h2>Downloads</h2>
+                            <p>${downloads}</p>
+                        </li>
+                    </ul>
+                </div>
                 
-//             </li>
-//         </div>
-//     </div>`).join("");
+            </li>
+        </div>
+    </div>`).join("");
 
-//     galleryList.insertAdjacentHTML('afterbegin', markup);
+    galleryList.insertAdjacentHTML('afterbegin', markup);
 
-//     new SimpleLightbox('.gallery a',
-//         {
-//             captionsData: 'alt',
-//             captionDelay: 250,
-//             captionPosition: 'bottom',
-//             widthRatio: 0.9,
-//             heightRatio: 0.8,
-//         });
-// }
+    new SimpleLightbox('.gallery a',
+        {
+            captionsData: 'alt',
+            captionDelay: 250,
+            captionPosition: 'bottom',
+            widthRatio: 0.9,
+            heightRatio: 0.8,
+        });
+}
